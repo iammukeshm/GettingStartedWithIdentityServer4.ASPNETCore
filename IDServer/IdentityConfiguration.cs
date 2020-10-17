2980b86c3b6e0ba090b8a3721918d997dccd295b
Mukesh Murugan <iammukeshm@gmail.com>
2020-10-17 18:38:04 +0530

    added webapi

---
+++ IDServer/IdentityConfiguration.cs
@@ -0,0 +1,73 @@
+﻿using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace IDServer
+{
+    public class IdentityConfiguration
+    {
+public static List<TestUser> TestUsers =>
+    new List<TestUser>
+    {
+        new TestUser
+        {
+            SubjectId = "1144",
+            Username = "mukesh",
+            Password = "mukesh",
+            Claims =
+            {
+                new Claim(JwtClaimTypes.Name, "Mukesh Murugan"),
+                new Claim(JwtClaimTypes.GivenName, "Mukesh"),
+                new Claim(JwtClaimTypes.FamilyName, "Murugan"),
+                new Claim(JwtClaimTypes.WebSite, "http://codewithmukesh.com"),
+            }
+        }
+};
+
+public static IEnumerable<IdentityResource> IdentityResources =>
+    new IdentityResource[]
+    {
+        new IdentityResources.OpenId(),
+        new IdentityResources.Profile(),
+    };
+
+public static IEnumerable<ApiScope> ApiScopes =>
+    new ApiScope[]
+    {
+        new ApiScope("myApi.read"),
+        new ApiScope("myApi.write"),
+    };
+public static IEnumerable<ApiResource> ApiResources =>
+    new ApiResource[]
+    {
+        new ApiResource("myApi")
+        {
+            Scopes = new List<string>{ "myApi.read","myApi.write" },
+            ApiSecrets = new List<Secret>{ new Secret("supersecret".Sha256()) }
+        }
+    };
+
+public static IEnumerable<Client> Clients =>
+    new Client[]
+    {
+        // m2m client credentials flow client
+        new Client
+        {
+            ClientId = "cwm.client",
+            ClientName = "Client Credentials Client",
+
+            AllowedGrantTypes = GrantTypes.ClientCredentials,
+            ClientSecrets = { new Secret("secret".Sha256()) },
+
+            AllowedScopes = { "myApi.read" }
+        },
+    };
+    }
+}
